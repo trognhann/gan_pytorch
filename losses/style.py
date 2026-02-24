26967592de4128272812827a9ee7f94ec04469f4
Trognhann <trognhann@Trognhanns-MacBook-Air.local>
2026-02-24 09:51:52 +0700

    fix sty nan

--- losses/style.py
+++ losses/style.py
@@ -12,32 +12,35 @@ class StyleLoss(nn.Module):
 
     def gram_matrix(self, x):
         b, c, h, w = x.size()
-        features = x.view(b, c, h * w)
-        G = torch.bmm(features, features.transpose(1, 2))
+        # MUST disable autocast, otherwise torch.bmm will automatically downcast float32 back to float16
+        with torch.amp.autocast('cuda', enabled=False):
+            features = x.view(b, c, h * w).to(torch.float32)
+            G = torch.bmm(features, features.transpose(1, 2))
         return G / (c * h * w)
 
     def forward(self, style, fake):
         s2, s3, s4 = self.vgg(style)
         f2, f3, f4 = self.vgg(fake)
 
-        s2, f2 = s2.to(torch.float32), f2.to(torch.float32)
-        s3, f3 = s3.to(torch.float32), f3.to(torch.float32)
-        s4, f4 = s4.to(torch.float32), f4.to(torch.float32)
+        with torch.amp.autocast('cuda', enabled=False):
+            s2, f2 = s2.to(torch.float32), f2.to(torch.float32)
+            s3, f3 = s3.to(torch.float32), f3.to(torch.float32)
+            s4, f4 = s4.to(torch.float32), f4.to(torch.float32)
 
-        s2 = s2 - s2.mean(dim=[2, 3], keepdim=True)
-        f2 = f2 - f2.mean(dim=[2, 3], keepdim=True)
+            s2 = s2 - s2.mean(dim=[2, 3], keepdim=True)
+            f2 = f2 - f2.mean(dim=[2, 3], keepdim=True)
 
-        s3 = s3 - s3.mean(dim=[2, 3], keepdim=True)
-        f3 = f3 - f3.mean(dim=[2, 3], keepdim=True)
+            s3 = s3 - s3.mean(dim=[2, 3], keepdim=True)
+            f3 = f3 - f3.mean(dim=[2, 3], keepdim=True)
 
-        s4 = s4 - s4.mean(dim=[2, 3], keepdim=True)
-        f4 = f4 - f4.mean(dim=[2, 3], keepdim=True)
+            s4 = s4 - s4.mean(dim=[2, 3], keepdim=True)
+            f4 = f4 - f4.mean(dim=[2, 3], keepdim=True)
 
-        l2 = self.weights[0] * self.l1(self.gram_matrix(s2),
-                                       self.gram_matrix(f2)) / s2.size(1)
-        l3 = self.weights[1] * self.l1(self.gram_matrix(s3),
-                                       self.gram_matrix(f3)) / s3.size(1)
-        l4 = self.weights[2] * self.l1(self.gram_matrix(s4),
-                                       self.gram_matrix(f4)) / s4.size(1)
+            l2 = self.weights[0] * self.l1(self.gram_matrix(s2),
+                                           self.gram_matrix(f2)) / s2.size(1)
+            l3 = self.weights[1] * self.l1(self.gram_matrix(s3),
+                                           self.gram_matrix(f3)) / s3.size(1)
+            l4 = self.weights[2] * self.l1(self.gram_matrix(s4),
+                                           self.gram_matrix(f4)) / s4.size(1)
 
         return l2 + l3 + l4
